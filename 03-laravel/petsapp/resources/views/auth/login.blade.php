e23b6d3c69d0bb0e2ca9fca514ffb0ad169e5ec8
sullivan <cochiscoso@gmail.com>
2025-04-10 11:50:08 -0500

    se creo interfas login register

--- 03-laravel/petsapp/resources/views/auth/login.blade.php
+++ 03-laravel/petsapp/resources/views/auth/login.blade.php
@@ -45,4 +45,23 @@
         </div>
     </form>
 </x-guest-layout> --}}
-ola
+
+
+@extends('layouts.app')
+@section('title', 'Login PetsApp')
+@section('content')
+<fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+    <h1 class="text-2xl text-center flex justify-center items-center gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+        <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
+        </svg>  
+    Login</h1>
+    <label class="fieldset-label">Email</label>
+    <input type="email" class="input" placeholder="Email" />
+    
+    <label class="fieldset-label">Password</label>
+    <input type="password" class="input" placeholder="Password" />
+    
+    <button class="btn mt-4 bg-cyan-800 text-white rounded-full ">Login</button>
+  </fieldset>
+@endsection
