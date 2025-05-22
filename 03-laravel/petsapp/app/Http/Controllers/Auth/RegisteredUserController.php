1f828a661b5c9a49c76145faca7ae3d07c77e560
sullivan <cochiscoso@gmail.com>
2025-05-22 07:09:40 -0500

    se edito login

--- 03-laravel/petsapp/app/Http/Controllers/Auth/RegisteredUserController.php
+++ 03-laravel/petsapp/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,9 +30,10 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'document' => ['required', 'string', 'numeric'],
+            'fullname' => ['required', 'string'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed'],
         ]);
 
         $user = User::create([
--- 03-laravel/petsapp/resources/views/auth/login.blade.php
+++ 03-laravel/petsapp/resources/views/auth/login.blade.php
@@ -48,23 +48,35 @@
 
 
 @extends('layouts.app')
-@section('title', 'Login PetsApp')
+@section('title', 'Login - PetsApp')
 @section('content')
-<form method="POST" action="{{route('login')}}">
-    @csrf
-<fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
-    <h1 class="text-2xl text-center flex justify-center items-center gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-        <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
-        </svg>  
-    Login</h1>
-    <label class="fieldset-label">Email</label>
-    <input type="email" class="input" placeholder="Email" />
-    
-    <label class="fieldset-label">Password</label>
-    <input type="password" class="input" placeholder="Password" />
-    
-    <button class="btn mt-4 bg-cyan-800 text-white rounded-full ">Login</button>
-  </fieldset>
-</form>
+    @include('layouts.navbar')
+    <form method="post" action="{{ route('login') }}">
+        @csrf
+        <fieldset class="fieldset w-sm bg-base-200 border border-base-300 p-4 rounded-box">
+                <h1 class="text-2xl text-center flex justify-center items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                    </svg> 
+                    Login
+                </h1>
+                @if (count($errors->all()) > 0)
+                    <div class="flex flex-col gap-1 my-4">
+                        @foreach ($errors->all() as $message)
+                            <div class="badge badge-error">
+                                <svg class="size-[1em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor"><rect x="1.972" y="11" width="20.056" height="2" transform="translate(-4.971 12) rotate(-45)" fill="currentColor" stroke-width="0"></rect><path d="m12,23c-6.065,0-11-4.935-11-11S5.935,1,12,1s11,4.935,11,11-4.935,11-11,11Zm0-20C7.038,3,3,7.037,3,12s4.038,9,9,9,9-4.037,9-9S16.962,3,12,3Z" stroke-width="0" fill="currentColor"></path></g></svg>
+                                {{ $message }}
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+                <label class="fieldset-label">Email</label>
+                <input type="email" name="email" class="input rounded-full" placeholder="Email" />
+                
+                <label class="fieldset-label">Password</label>
+                <input type="password" name="password" class="input rounded-full" placeholder="Password" />
+                
+                <button class="btn mt-4 rounded-full text-white bg-cyan-800">Login</button>
+        </fieldset>
+    </form>
 @endsection
